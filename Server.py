69bb6a6589ac30eb96cdcb6a23352acb6af48060
Abhinav Rana <vibranereaper@gmail.com>
2018-08-22 09:04:08 -0700

    Dummy Server working :)

--- Server.py
+++ Server.py
@@ -1,42 +1,45 @@
-#!/usr/bin/env python  
-  
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer  
-import os  
-  
-#Create custom HTTPRequestHandler class  
-class AbhiHTTPRequestHandler(BaseHTTPRequestHandler):  
+#!/usr/bin/env python
+"""
+Very simple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-  #handle GET command  
-  def do_GET(self):  
-    rootdir = '/' #file location  
-    try:  
-      if self.path.endswith('.html'):  
-        f = open(rootdir + self.path) #open requested file  
-  
-        #send code 200 response  
-        self.send_response(200)  
-  
-        #send header first  
-        self.send_header('Content-type','text-html')  
-        self.end_headers()  
-  
-        #send file content to client  
-        self.wfile.write(f.read())  
-        f.close()  
-        return  
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-    except IOError:  
-      self.send_error(404, 'file not found')  
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
 
-def run():  
-  print('http server is starting...')  
-  
-  #ip and port of servr  
-  #by default http server port is 80  
-  server_address = ('127.0.0.1', 80)  
-  httpd = HTTPServer(server_address, AbhiHTTPRequestHandler)  
-  print('http server is running...')  
-  httpd.serve_forever()  
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
 
-if __name__ == '__main__':  
-  run() 
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
