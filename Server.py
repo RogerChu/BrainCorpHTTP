428b32c52ba59e0dfecc8904baa228af3ebb42ae
Abhinav Rana <vibranereaper@gmail.com>
2018-09-01 11:33:25 -0700

    Understand URL Encoding for Queries

--- Server.py
+++ Server.py
@@ -7,8 +7,9 @@ Send a GET request::
     curl http://localhost
 """
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os.path, time
-
+import os.path
+import time
+from copy import deepcopy
 
 
 class S(BaseHTTPRequestHandler):
@@ -16,20 +17,17 @@ class S(BaseHTTPRequestHandler):
     passwd_time = None
     passwd_list = []
 
-
-
-    
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-    
+
     def passwd(self):
         current_time = os.stat('/etc/passwd').st_mtime
         if (S.passwd_time == None or current_time > S.passwd_time):
             self.parse_passwd()
             S.passwd_time = current_time
-            
+
     def parse_passwd(self):
 
         f = open('/etc/passwd', 'r')
@@ -39,57 +37,55 @@ class S(BaseHTTPRequestHandler):
         for line in lines:
             if '#' == line[0]:
                 continue
-            split = line.split(':')  
-            split[6] = split[6][0:len(split[6]) - 1]
+            split = line.split(':')
+            split.pop(1)
+            split[5] = split[5][0:len(split[5]) - 1]
             S.passwd_list.append(split)
         return
 
     def parse_path(self, path):
-        
+
         if path == '/users':
             return 'all'
+        elif '/users/query' in path:
+            return
 
         return
 
     def user_query(self, parameters):
-        #implement afterwards
+        # implement afterwards
+        passwd_list_copy = deepcopy(S.passwd_list)
         for param in parameters:
             continue
-        
-        
-        
 
-
-        return
+        layout = ['name: ', 'uid: ', 'gid: ', 'comment: ', 'home: ', 'shell: ']
+        encoded_list = []
+        for row in passwd_list_copy:
+            result = []
+            for index, elem in enumerate(row):
+                result.append(layout[index] + elem)
+            encoded_list.append(result)
+        return encoded_list
 
     def do_GET(self):
 
         option = self.parse_path(self.path)
 
+        encoded_list = None
         if option == 'all':
             self.passwd()
-            self.user_query([])
+            encoded_list = str(self.user_query([])).encode()
 
-        encoded_list = str(S.passwd_list).encode()
         self._set_headers()
         self.wfile.write(encoded_list)
 
 
-
-        # l = []
-        # l.append(10)
-        # l.append(20)
-        # l = str(l).encode()
-        # self.wfile.write(l)
-
-        # self.wfile.write("<html><body><h1>hi!</h1></body></html>".encode())
-        
-        
 def run(server_class=HTTPServer, handler_class=S, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print('Starting httpd on port: ' + str(port))
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     from sys import argv
